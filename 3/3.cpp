94de5582a8dcaa45198ba95dd321551c33060341
Alexandr Karkotski <fpm.karkocki@bsu.by>
2022-05-08 18:36:50 +0300

    Creating thread
    Writing the code of the thread.

--- 3/3.cpp
+++ 3/3.cpp
@@ -16,6 +16,38 @@ struct numsThread {
 };
 DWORD WINAPI marker(LPVOID _arrF)
 {
+	numsThread arrF = *(numsThread*)_arrF;
+	srand(arrF.num);
+	bool check = false;
+	int count = 0;
+	while(!check) {
+		int temp = rand();
+		temp = temp % n;
+		EnterCriticalSection(&criticalSection);
+		if (arrF.arr[temp] == 0) {
+			Sleep(5);
+			arrF.arr[temp] = arrF.num;
+			Sleep(5);
+			count += 1;
+		}
+		else {
+			cout << arrF.num << " " << count << " " << temp <<endl;			
+			LeaveCriticalSection(&criticalSection);
+			SetEvent(arrF.stop);
+			if (WaitForMultipleObjects(markerCount, hThread, FALSE, INFINITE) == WAIT_FAILED) {
+				cout << "Error." << endl;
+			}
+			int k = WaitForMultipleObjects(2, arrF.event, FALSE, INFINITE) - WAIT_OBJECT_0;
+			if (k == 0) {
+				check = true;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		if (arrF.arr[i] == arrF.num) {
+			arrF.arr[i] = 0;
+		}
+	}
 	return 0;
 }
 int main() {
