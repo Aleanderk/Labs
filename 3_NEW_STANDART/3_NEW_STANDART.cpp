e0b6a03ad9ab1e5736b8fa24dd430ea402803e74
Alexandr Karkotski <fpm.karkocki@bsu.by>
2022-05-08 20:54:25 +0300

    Creating thread

--- 3_NEW_STANDART/3_NEW_STANDART.cpp
+++ 3_NEW_STANDART/3_NEW_STANDART.cpp
@@ -17,7 +17,38 @@ int number = -1;
 
 void marker(int num)
 {
-	
+srand(num);
+	int count = 0;
+	std::vector<int> mass;
+	while (true) {
+		std::unique_lock<std::mutex> ul(signal);
+		if (number == num) {
+			checkArr[num] = false;
+			for (int i = 0; i < mass.size(); i++) {
+				arr[mass[i]] = 0;
+			}
+			
+			con.notify_all();
+			ul.unlock();
+			return;
+		}
+		int temp = rand() % n;
+		if (arr[temp] == 0) {
+			Sleep(5);
+			arr[temp] = num + 1;
+			if (!check(mass, temp)) {
+				mass.push_back(temp);
+			}
+			Sleep(5);
+		}
+		else {
+			checkArr[num] = false;
+			cout << num << " " << mass.size() << " " << temp << endl;
+			con.notify_all();
+			con.wait(ul, [=]() {return checkArr[num]; });
+		}
+		ul.unlock();
+	}	
 }
 int main() {	
 	cin >> n;
