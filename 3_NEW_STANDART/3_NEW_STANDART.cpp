3225d41a8ad24f230ae10af6f060a1b4c4857c7c
Alexandr Karkotski <fpm.karkocki@bsu.by>
2022-05-08 20:56:03 +0300

    Output

--- 3_NEW_STANDART/3_NEW_STANDART.cpp
+++ 3_NEW_STANDART/3_NEW_STANDART.cpp
@@ -14,10 +14,25 @@ int markerCount;
 std::mutex signal;
 std::condition_variable con;
 int number = -1;
-
+bool StopThreads() {
+	for (int i = 0; i < markerCount; i++) {
+		if (checkArr[i]) {
+			return false;
+		}
+	}
+	return true;
+}
+bool check(const std::vector<int>& endOftheThreads, int i) {
+	for (int j = 0; j < endOftheThreads.size(); j++) {
+		if (endOftheThreads[j] == i) {
+			return true;
+		}
+	}
+	return false;
+}
 void marker(int num)
 {
-srand(num);
+	srand(num);
 	int count = 0;
 	std::vector<int> mass;
 	while (true) {
@@ -65,6 +80,39 @@ int main() {
 	}
 	for (int i = 0; i < markerCount; i++) {
 		hThreads[i].detach();
+	}
+	std::vector<int> endOftheThreads;
+	int temp = markerCount;
+	while (markerCount > 0) {
+		std::unique_lock<std::mutex> ul(signal);
+		con.wait(ul, [=]() {return StopThreads(); });
+		for (int i = 0; i < n; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+		int index;
+		cout << "Enter the number of the thread to be completed: ";
+		cin >> index;
+		index--;
+		if (index >= temp || index < 0) {
+			cout << "Input error." << endl;
+			break;
+		}
+		else {
+			endOftheThreads.push_back(index);
+			checkArr[n] = true;
+			number = n;
+			con.notify_all();
+			con.wait(ul, [=]() {return StopThreads(); });
+			for (int i = 0; i < markerCount; i++) {
+				if (!check(endOftheThreads, i)) {
+					checkArr[i] = true;
+				}
+			}
+			con.notify_all();
+			ul.unlock();
+			markerCount--;
+		}
 	}
 	return 0;
 }
